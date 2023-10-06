39010f2e626eeadcb1b8326db22cc52900860220
Paprika <aserstritt@gmail.com>
2023-10-06 23:52:58 +0800

    start

---
+++ app/src/Activity/GeneralActivity.java
@@ -0,0 +1,12 @@
+package Activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+public class GeneralActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+    }
+}
